44200f08debc75cca091bcf7b96416dd50c44d39
Andrew Valencik <andrew.valencik@gmail.com>
2017-12-15 10:21:08 -0500

    Finish part two

--- src/main/scala/ca/valencik/DaySeven.scala
+++ src/main/scala/ca/valencik/DaySeven.scala
@@ -4,7 +4,47 @@ import ca.valencik.Utils.putStrLn
 
 object DaySeven extends App {
 
-  case class TreeNode(name: String, weight: String, children: List[String])
+  def maybeIndexOfOddBall[A](xs: List[A]): Option[Int] = {
+    if (xs.length < 3)
+      None
+    else {
+      val three = xs.take(3)
+      if (three.distinct.length == 1) {
+        val oddIndex = xs.indexWhere(_ != xs.head)
+        if (oddIndex >= 0) Some(oddIndex) else None
+      } else {
+        val oddValue =
+          three.groupBy(identity).mapValues(_.length).filter(_._2 == 1).head._1
+        Some(xs.indexOf(oddValue))
+      }
+    }
+  }
+
+  case class TreeProper(name: String, weight: Int, children: List[TreeProper]) {
+    def sumWeight(): Int = {
+      def innerWeight(acc: Int, kids: List[TreeProper]): Int = {
+        if (kids.length == 0)
+          acc
+        else
+          innerWeight(acc + kids.map(_.weight).sum, kids.flatMap(_.children))
+      }
+      innerWeight(weight, children)
+    }
+    lazy val childWeights                 = children.map(_.sumWeight)
+    lazy val numChildren                  = children.length
+    lazy val hasChildrenBalanced: Boolean = childWeights.distinct.length == 1
+    lazy val unbalancedChild: Option[TreeProper] = {
+      maybeIndexOfOddBall(childWeights) match {
+        case Some(i) => Some(children(i))
+        case None    => None
+      }
+    }
+    lazy val balancedChild = children.find(_.hasChildrenBalanced)
+  }
+
+  case class TreeNode(name: String, weight: String, children: List[String]) {
+    def getWeight(): Int = weight.trim.stripPrefix("(").stripSuffix(")").toInt
+  }
 
   def splitTree(treeString: String): List[String] = {
     treeString.split(",?\\s+").toList
@@ -36,6 +76,33 @@ object DaySeven extends App {
     inner(start)
   }
 
+  def constructTreeProperFromTreeNodes(tns: List[TreeNode]): TreeProper = {
+    val root = recurseToRoot(tns.head, tns)
+    def innerConstruct(tn: TreeNode): TreeProper = {
+      if (tn.children.isEmpty)
+        TreeProper(tn.name, tn.getWeight, List())
+      else
+        TreeProper(
+          tn.name,
+          tn.getWeight,
+          tns.filter(tn.children contains _.name).map(innerConstruct)
+        )
+    }
+    innerConstruct(root)
+  }
+
+  def balanceTree(root: TreeProper): Option[Int] = {
+    def inner(grandParent: TreeProper, parent: TreeProper): Option[Int] = {
+      parent.unbalancedChild match {
+        case Some(c) => inner(parent, c)
+        case None =>
+          Some(
+            grandParent.balancedChild.get.sumWeight - parent.childWeights.sum)
+      }
+    }
+    inner(root, root)
+  }
+
   def process(input: String): Iterator[String] = {
     scala.io.Source.fromFile(input).getLines()
   }
@@ -47,5 +114,12 @@ object DaySeven extends App {
     root.name
   }
 
+  def partTwo(lines: Iterator[String]): Int = {
+    val treeNodes = genTreeNodes(lines)
+    val root      = constructTreeProperFromTreeNodes(treeNodes)
+    balanceTree(root).get
+  }
+
   putStrLn("partOne: " + partOne(process(args(0))))
+  putStrLn("partTwo: " + partTwo(process(args(0))))
 }
--- src/test/scala/ca/valencik/DaySevenSpec.scala
+++ src/test/scala/ca/valencik/DaySevenSpec.scala
@@ -3,8 +3,8 @@ package ca.valencik
 import org.scalatest._
 
 class DaySevenSpec extends FlatSpec with Matchers {
-  "DaySeven partOne tree" should "return tknk" in {
-    DaySeven.partOne("""pbga (66)
+
+  def exampleTree: Iterator[String] = """pbga (66)
                  |xhth (57)
                  |ebii (61)
                  |havc (66)
@@ -16,7 +16,29 @@ class DaySevenSpec extends FlatSpec with Matchers {
                  |jptl (61)
                  |ugml (68) -> gyxo, ebii, jptl
                  |gyxo (61)
-                 |cntj (57)""".stripMargin.lines) shouldEqual "tknk"
+                 |cntj (57)""".stripMargin.lines
+
+  "DaySeven partOne tree" should "return tknk" in {
+    DaySeven.partOne(exampleTree) shouldEqual "tknk"
+  }
+
+  "DaySeven childless TreeProper sumWeight" should "return own weight" in {
+    val t = DaySeven.TreeProper("test", 1, List())
+    t.sumWeight shouldEqual 1
+  }
+
+  "DaySeven TreeProper sumWeight" should "return sum of own weight and children" in {
+    val t = DaySeven.TreeProper("test",
+                                1,
+                                List(DaySeven.TreeProper("test1", 1, List()),
+                                     DaySeven.TreeProper("test2", 2, List()),
+                                     DaySeven.TreeProper("test3", 3, List())))
+    t.sumWeight shouldEqual 7
+  }
+
+  "DaySeven partTwo tree" should "return 60" in {
+    DaySeven.partTwo(exampleTree) shouldEqual 60
+
   }
 
 }
