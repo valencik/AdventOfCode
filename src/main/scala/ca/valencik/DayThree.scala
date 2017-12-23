003d248beda8da787bb11e5fdc2b9bbd8fefd002
Andrew Valencik <andrew.valencik@gmail.com>
2017-12-22 21:07:49 -0500

    Partial work on Day3 partTwo

--- src/main/scala/ca/valencik/DayThree.scala
+++ src/main/scala/ca/valencik/DayThree.scala
@@ -1,7 +1,7 @@
 package ca.valencik
 
 import ca.valencik.Utils.putStrLn
-import Math.abs
+import Math.{abs, max, min}
 
 object DayThree extends App {
   def ssize(n: Int): Int = {
@@ -15,6 +15,7 @@ object DayThree extends App {
     inner(1)
   }
 
+  case class Compass(e: Int, n: Int, w: Int, s: Int)
   def compassPoints(r: Int): List[Int] = {
     val e = 4 * (r * r) - 11 * r + 8
     val n = 4 * (r * r) - 9 * r + 6
@@ -25,6 +26,28 @@ object DayThree extends App {
 
   def closestPoint(ps: List[Int], n: Int): Int = {
     ps.map(_ - n).map(Math.abs).min
+  }
+
+  case class Point(x: Int, y: Int) {
+    lazy val quadrant = {
+      if (x >= 0 && y >= 0) 1
+      else if (x < 0 && y >= 0) 2
+      else if (x < 0 && y < 0) 3
+      else 4
+    }
+    lazy val r      = ring(max(abs(x), abs(y)))
+    lazy val cp     = compassPoints(r + 1)
+    lazy val comp   = Compass(cp(0), cp(1), cp(2), cp(3))
+    lazy val majorX = abs(x) > abs(y)
+    lazy val majorY = abs(y) >= abs(x)
+    def spiralNum: Int = {
+      if (x == 0 && y == 0) 1
+      else if (quadrant == 1) if (majorX) comp.e + y else comp.n - x
+      else if (quadrant == 2) if (majorX) comp.w - y else comp.n - x
+      else if (quadrant == 3) if (majorX) comp.w - y else comp.s + x
+      else if (majorX) comp.e + y
+      else comp.s + x
+    }
   }
 
   def partOne(numString: String): Int = {
--- src/test/scala/ca/valencik/DayThreeSpec.scala
+++ src/test/scala/ca/valencik/DayThreeSpec.scala
@@ -16,4 +16,40 @@ class DayThreeSpec extends FlatSpec with Matchers {
     DayThree.partOne("1024") shouldEqual 31
   }
 
+  "DayThree Point(1,1)" should "work" in {
+    DayThree.Point(1, 1).spiralNum shouldEqual 3
+    DayThree.Point(-1, 1).spiralNum shouldEqual 5
+    DayThree.Point(-1, -1).spiralNum shouldEqual 7
+    DayThree.Point(1, -1).spiralNum shouldEqual 9
+  }
+  "DayThree Point(2,2)" should "work" in {
+    DayThree.Point(2, 2).spiralNum shouldEqual 13
+    DayThree.Point(-2, 2).spiralNum shouldEqual 17
+    DayThree.Point(-2, -2).spiralNum shouldEqual 21
+    DayThree.Point(2, -2).spiralNum shouldEqual 25
+  }
+  "DayThree Point(2,1)" should "work" in {
+    DayThree.Point(2, 1).spiralNum shouldEqual 12
+    DayThree.Point(-2, 1).spiralNum shouldEqual 18
+    DayThree.Point(-2, -1).spiralNum shouldEqual 20
+    DayThree.Point(2, -1).spiralNum shouldEqual 10
+  }
+  "DayThree Point(1,2)" should "work" in {
+    DayThree.Point(1, 2).spiralNum shouldEqual 14
+    DayThree.Point(-1, 2).spiralNum shouldEqual 16
+    DayThree.Point(-1, -2).spiralNum shouldEqual 22
+    DayThree.Point(1, -2).spiralNum shouldEqual 24
+  }
+  "DayThree incrementing Points" should "work" in {
+    DayThree.Point(0, 0).spiralNum shouldEqual 1
+    DayThree.Point(1, 0).spiralNum shouldEqual 2
+    DayThree.Point(1, 1).spiralNum shouldEqual 3
+    DayThree.Point(0, 1).spiralNum shouldEqual 4
+    DayThree.Point(-1, 1).spiralNum shouldEqual 5
+    DayThree.Point(-1, 0).spiralNum shouldEqual 6
+    DayThree.Point(-1, -1).spiralNum shouldEqual 7
+    DayThree.Point(0, -1).spiralNum shouldEqual 8
+    DayThree.Point(1, -1).spiralNum shouldEqual 9
+  }
+
 }
