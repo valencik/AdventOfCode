638ff4e74d2dd85bda973510eabac7c5cd2d32de
Andrew Valencik <andrew.valencik@gmail.com>
2018-08-07 18:06:57 -0400

    Add solution to DayTen partTwo

--- src/main/scala/ca/valencik/DayTen.scala
+++ src/main/scala/ca/valencik/DayTen.scala
@@ -1,6 +1,7 @@
 package ca.valencik
 
 import ca.valencik.Utils.putStrLn
+import scala.annotation.tailrec
 
 object DayTen extends App {
 
@@ -24,6 +25,12 @@ object DayTen extends App {
       lengths.foldLeft(this) { case (circ, i) => circ.hash(i) }
     }
     lazy val productOfFirstTwo = xs.take(2).product
+
+    def densifyHash(bits: List[Int]): Iterator[Int] =
+      bits
+        .grouped(16)
+        .map(_.foldLeft(0) { case (acc, i) => acc ^ i })
+    lazy val denseHash = densifyHash(this.collect())
   }
   case object CircList {
     def apply(n: Int): CircList = {
@@ -32,8 +39,34 @@ object DayTen extends App {
     }
   }
 
-  val lengthList = args.head.split(",").toList.map(_.toInt)
-  val circ       = CircList(256).hashList(lengthList)
-  putStrLn(circ.productOfFirstTwo.toString)
+  def multiRoundHash(circ: CircList, rounds: Int)(
+      lengthList: List[Int]): CircList = {
+    @tailrec
+    def loop(circ: CircList, rounds: Int): CircList = {
+      if (rounds <= 0)
+        circ
+      else
+        loop(circ.hashList(lengthList), rounds - 1)
+    }
+    loop(circ, rounds)
+  }
+
+  def partOne(input: String): Int = {
+    val lengthList = input.split(",").toList.map(_.toInt)
+    val circ       = CircList(256).hashList(lengthList)
+    circ.productOfFirstTwo
+  }
+
+  def partTwo(input: String): String = {
+    val standardLengthSuffix = List(17, 31, 73, 47, 23)
+    val lengthList2          = input.getBytes.toList.map(_.toInt) ++ standardLengthSuffix
+    val sparseHash           = multiRoundHash(CircList(256), 64)(lengthList2)
+    val denseHash            = sparseHash.denseHash
+    val hexString            = denseHash.map("%02x".format(_)).mkString
+    hexString
+  }
+
+  putStrLn(partOne(args.head).toString)
+  putStrLn(partTwo(args.head))
 
 }
