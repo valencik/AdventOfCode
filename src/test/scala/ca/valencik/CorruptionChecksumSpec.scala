f6d73d243718910029e5384e24f2ebfb5f67c8a0
Andrew Valencik <andrew.valencik@gmail.com>
2018-12-03 21:59:01 -0500

    Rename DayTwo to CorruptionChecksum

--- src/test/scala/ca/valencik/CorruptionChecksumSpec.scala
+++ src/test/scala/ca/valencik/CorruptionChecksumSpec.scala
@@ -2,9 +2,9 @@ package ca.valencik
 
 import org.scalatest._
 
-class DayTwoSpec extends FlatSpec with Matchers {
-  "DayTwo" should "implement partOne grid" in {
-    DayTwo.partOne("""
+class CorruptionChecksumSpec extends FlatSpec with Matchers {
+  "CorruptionChecksum" should "implement partOne grid" in {
+    CorruptionChecksum.partOne("""
       5 1 9 5
       7 5 3
       2 4 6 8
@@ -12,7 +12,7 @@ class DayTwoSpec extends FlatSpec with Matchers {
   }
 
   it should "implement partTwo grid" in {
-    DayTwo.partTwo("""
+    CorruptionChecksum.partTwo("""
       5 9 2 8
       9 4 7 3
       3 8 6 5
