04aea228a9ede57096839000f46f856d9b1e19e5
Andrew Valencik <andrew.valencik@gmail.com>
2018-12-04 22:50:15 -0500

    Add partTwo to ChronalCalibrator

--- src/main/scala/ca/valencik/ChronalCalibrator.scala
+++ src/main/scala/ca/valencik/ChronalCalibrator.scala
@@ -4,18 +4,36 @@ import ca.valencik.Utils.putStrLn
 
 object ChronalCalibrator extends App {
 
-  def sumFreq(freqs: Iterator[String]): Int = {
-    freqs.map(_.toInt).sum
+  def process(input: String): Iterable[String] = {
+    scala.io.Source.fromFile(input).getLines().toIterable
   }
 
-  def process(input: String): Iterator[String] = {
-    scala.io.Source.fromFile(input).getLines()
+  def partOne(lines: Iterable[String]): Int = {
+    lines.map(_.toInt).sum
   }
 
-  def partOne(lines: Iterator[String]): Int = {
-    sumFreq(lines)
+  def infiniteStream[A](xs: Iterable[A]): Stream[A] = {
+    lazy val ss: Stream[A] = xs.toStream #::: ss
+    ss
+  }
+
+  def firstDuplicate[A](xs: Iterable[A]): A = {
+    var these: Iterable[A] = xs
+    var seen: Set[A]       = Set()
+    while (!seen(these.head)) {
+      seen += these.head
+      these = these.tail
+    }
+    these.head
+  }
+
+  def partTwo(lines: Iterable[String]): Int = {
+    val inf    = infiniteStream(lines.map(_.toInt))
+    val deltas = inf.scanLeft(0)(_ + _)
+    firstDuplicate(deltas)
   }
 
   putStrLn("partOne: " + partOne(process(args(0))))
+  putStrLn("partTwo: " + partTwo(process(args(0))))
 
 }
--- src/test/scala/ca/valencik/ChronalCalibratorSpec.scala
+++ src/test/scala/ca/valencik/ChronalCalibratorSpec.scala
@@ -4,13 +4,20 @@ import org.scalatest._
 
 class ChronalCalibratorSpec extends FlatSpec with Matchers {
 
-  "ChronalCalibrator.sumFreq" should "sum number strings" in {
-    val nums = List("1", "2", "3").toIterator
-    ChronalCalibrator.sumFreq(nums) shouldBe 6
+  "ChronalCalibrator partOne" should "sum number strings" in {
+    val nums = List("1", "2", "3")
+    ChronalCalibrator.partOne(nums) shouldBe 6
   }
 
   it should "sum signed number strings" in {
-    val nums = List("+1", "-2", "+3").toIterator
-    ChronalCalibrator.sumFreq(nums) shouldBe 2
+    val nums = List("+1", "-2", "+3")
+    ChronalCalibrator.partOne(nums) shouldBe 2
+  }
+
+  "ChronalCalibrator partTwo" should "return the first duplicate state" in {
+    ChronalCalibrator.partTwo(Seq("+1", "-1")) shouldBe 0
+    ChronalCalibrator.partTwo(Seq("+3", "+3", "+4", "-2", "-4")) shouldBe 10
+    ChronalCalibrator.partTwo(Seq("-6", "+3", "+8", "+5", "-6")) shouldBe 5
+    ChronalCalibrator.partTwo(Seq("+7", "+7", "-2", "-7", "-4")) shouldBe 14
   }
 }
