8befc6d4a4900dbc792779f26205d150e4901e67
Andrew Valencik <andrew.valencik@gmail.com>
2021-12-05 08:39:37 -0500

    Track turns in Bingo
    Co-authored-by: Sam Pillsworth <sam@blerf.ca>

--- modules/aoc/src/main/scala/ca/valencik/aoc/y2021/Day04.scala
+++ modules/aoc/src/main/scala/ca/valencik/aoc/y2021/Day04.scala
@@ -10,13 +10,14 @@ import fs2.{Pipe, Stream, INothing, text}
 import fs2.concurrent.Topic
 import ca.valencik.aoc.AdventApp
 
-case class Bingo private (nums: List[List[Int]], called: List[(Int, Int)]):
+case class Bingo private (nums: List[List[Int]], turns: Int, called: List[(Int, Int)]):
   def dab(n: Int): Bingo =
     val r1: Int = nums.indexWhere(row => row.contains(n))
     if (r1 >= 0) {
       val c1: Int = nums(r1).indexOf(n)
-      this.copy(called = called.prepended((r1, c1)))
-    } else this
+      this.copy(turns = turns + 1, called = called.prepended((r1, c1)))
+    } else if (nums.isEmpty) this
+    else this.copy(turns = turns + 1)
 
   val didWin: Boolean =
     val rowWin: Boolean = called.groupMapReduce(_._1)(_ => 1)(_ + _).valuesIterator.contains(5)
@@ -29,8 +30,10 @@ case class Bingo private (nums: List[List[Int]], called: List[(Int, Int)]):
 
   lazy val lastDab = called.head.pipe(nums(_)(_))
 
-  def callBingo: Int =
-    sumOfUncalled * lastDab
+  def callBingo: (Int, Int) =
+    (turns, sumOfUncalled * lastDab)
+
+  def empty: Bingo = this.copy(nums = List.empty, called = List.empty)
 
   override def toString: String =
     nums.map(ns => ns.map(n => f"$n%2d").mkString(" ")).mkString("\n")
@@ -38,18 +41,16 @@ case class Bingo private (nums: List[List[Int]], called: List[(Int, Int)]):
 object Bingo:
   def apply(rows: List[String]): Bingo =
     val nums: List[List[String]] = rows.map(_.split(" +").filterNot(_.isEmpty).toList)
-    Bingo(nums.map(r => r.map(_.trim.toInt)), List.empty)
+    Bingo(nums.map(r => r.map(_.trim.toInt)), 0, List.empty)
 
-  def empty: Bingo = Bingo(List.empty, List.empty)
-
-class EventService[F[_]](calls: Topic[F, Int], wins: Topic[F, Int])(
+class EventService[F[_]](calls: Topic[F, Int], wins: Topic[F, (Int, Int)])(
     nums: Stream[F, Int],
     boards: Stream[F, Bingo]
 )(implicit F: Temporal[F]):
 
   def startPublisher: Stream[F, Unit] =
-    Stream.sleep(0.5.second).drain ++ Stream
-      .awakeEvery[F](0.15.second)
+    Stream.sleep(0.5.second) ++ Stream
+      .awakeEvery[F](0.01.second)
       .zipRight(nums)
       .through(calls.publish)
 
@@ -59,7 +60,7 @@ class EventService[F[_]](calls: Topic[F, Int], wins: Topic[F, Int])(
       if (newB.didWin)
         wins
           .publish1(newB.callBingo)
-          .map(_ => Bingo.empty)
+          .map(_ => newB.empty)
       else F.pure(newB)
 
     val events: Stream[F, Int] = calls.subscribe(100)
@@ -68,9 +69,9 @@ class EventService[F[_]](calls: Topic[F, Int], wins: Topic[F, Int])(
       .parJoinUnbounded
 
 object Day04 extends AdventApp(2021, 4):
-  def program[F[_]: Async](in: Stream[F, String]): Stream[F, Int] = for {
+  def program[F[_]: Async](in: Stream[F, String]): Stream[F, (Int, Int)] = for {
     calls <- Stream.eval(Topic[F, Int])
-    wins  <- Stream.eval(Topic[F, Int])
+    wins  <- Stream.eval(Topic[F, (Int, Int)])
     lines   = in.through(text.lines)
     nums    = lines.take(1).flatMap(s => Stream.emits(s.split(",").map(_.toInt)))
     bs      = lines.drop(2).sliding(5, 6).map(c => Bingo(c.toList))
@@ -80,6 +81,6 @@ object Day04 extends AdventApp(2021, 4):
   } yield res
 
   override def part1[F[_]: Async](in: Stream[F, String]): F[String] =
-    program(in).take(1).map(_.toString).compile.toList.map(_.head)
+    program(in).compile.toList.map(_.sortBy(_._1).head.pipe(_._2.toString))
   override def part2[F[_]: Async](in: Stream[F, String]): F[String] =
-    program(in).takeRight(1).map(_.toString).compile.toList.map(_.head)
+    program(in).compile.toList.map(_.sortBy(-_._1).head.pipe(_._2.toString))
